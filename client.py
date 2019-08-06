158a579ca328ed70300fef9b0ea17a7ae7126b86
amanshaw4511 <51832938+amanshaw4511@users.noreply.github.com>
2019-08-07 00:36:05 +0530

    chat over wifi and remote acess

---
+++ client.py
@@ -0,0 +1,257 @@
+import socket
+import threading
+import subprocess
+import os
+
+# import pathlib
+
+BUFFER = 2048
+
+
+# ansi escape sequence color
+
+class Client(socket.socket):
+    def __init__(self, *args, **kwargs):
+        self.ip = input("host address :").strip()
+        self.port = 6789
+        if self.ip == "":
+            if os.name == 'posix':
+                self.ip = "192.168.43.66"  # default address of windows hostspot
+            elif os.name == 'nt':
+                self.ip = "192.168.43.1"  # default address of android hotspot
+            # os.name = input("enter ip:")
+        self.ip = "192.168.248.1"
+        try:
+
+            print("[+] creating socket")
+            socket.socket.__init__(self, *args, **kwargs)
+            print("--- socket created successfully")
+        except socket.error as e:
+            print("[-] Error Creating Socket :", str(e))
+
+    def connectHost(self):
+        print("[+] connecting to host: ", self.ip)
+        while True:
+            try:
+                self.connect((self.ip, self.port))
+                print("--- connetion successfull")
+                break
+            except socket.error as e:
+                pass
+
+    def sendm(self, msg):
+        try:
+            self.send(str.encode(msg))  # encode msg to binary and send
+        except socket.error as e:
+            print("[-] Error Sending Message/Command", str(e))
+
+    def recvm(self):
+        try:
+            msg = self.recv(BUFFER)  # receive msg
+            return msg.decode("utf-8")  # convert binary msg to str and return
+        except socket.error as e:
+            print("[-] Error Receiving Message/Command", str(e))
+
+    def download(self, fileName):
+        downloadedSize = 0
+        try:
+            file = open(fileName, "wb")  # open file for writing data
+        except Exception as e:
+            print("[+} Error Opening ", fileName, ":", str(e))
+            return
+        while True:
+            try:
+                data = self.recv(BUFFER)  # receive data
+                downloadedSize += 2
+                print("downloading", downloadedSize, "KB")
+                if data[-3:] == str.encode("~~~"):  # check if download complete command(check EOF)
+                    file.write(data[:-3])  # write data except extra EOF command
+                    print("download successfull")
+                    file.close()
+                    return
+                file.write(data)  # write data to file
+            except socket.error as e:
+                print("[-] Error Downloading File :", str(e))
+                return
+            except Exception as e:
+                print("[-] Error Writing Data :", str(e))
+                return
+
+    def upload(self, fileName):
+        uploadedSize = 0
+        try:
+            file = open(fileName, "rb")  # open file for reading data
+        except Exception as e:
+            print("[-} Error Opening", fileName, ":", str(e))
+            return
+        while True:
+            try:
+                data = file.read(BUFFER)  # read data from file
+                if not data:  # check if upload complete
+                    self.send(str.encode("~~~"))  # send EOF ( upload complete command)
+                    print("upload successfull")
+                    file.close()
+                    break
+                self.send(data)  # send data
+                uploadedSize += 2
+                print("uploading", uploadedSize, "KB")
+            except socket.error as e:
+                print("[-] Error Uploading File :", str(e))
+            except Exception as e:
+                print("[-] Error Reading Data :", str(e))
+                return
+
+
+def chatting(client):
+    def send(stopConversation):
+        while True:
+            msg = input().strip()
+            # check if conversation if ended
+            if stopConversation[0]:
+                break
+            elif msg == "quit":
+                client.sendm(msg)
+                # client.close()
+                stopConversation[0] = True
+                break
+            else:
+                client.sendm(msg)
+
+    def recv(stopConversation):
+        while True:
+            # check if conversation if ended
+            if stopConversation[0]:
+                print("conversation stopped")
+                break
+            msg = client.recvm()
+            if msg == "quit":
+                client.sendm("quit")  # sent quit back msg
+                # client.close()
+                stopConversation[0] = True
+                print("conversation stopped")
+                break
+            else:
+                print("\033[46m" + msg)  # "\033[46m" -> show green background text
+                # print("\033[44m")        # "\033[44m" -> show blue background text
+
+    # there are two thread
+    # one for sending msg (t_send) and one for receiving msg (t_recv)
+    stopConversation = [False]  # flag to stop thread (list to make it mutable so that called by ref.)
+    t_send = threading.Thread(target=send, args=(stopConversation,))
+    t_recv = threading.Thread(target=recv, args=(stopConversation,))
+    t_send.start()
+    t_recv.start()
+    while not stopConversation[0]:
+        pass
+    return
+
+
+def remoteAccess(client, mode):
+    def masterMode():
+        while True:
+            command = input().strip()
+            if command == "quit":
+                client.sendm(command)
+                break
+            elif command[:4] == "down":
+                client.sendm(command)
+                # fileName = pathlib.Path("C:/Users/Aman/Desktop") / command[5:]
+                client.download(command[5:])
+            elif command[:6] == "upload":
+                client.upload(command[7:])
+            else:
+                client.sendm(command)
+                print(client.recvm())
+
+    def slaveMode():
+        while True:
+            command = client.recvm()
+            print(command)
+            if command == "quit":
+                break
+            elif command[:2] == "cd":
+                try:
+                    os.chdir(command[3:])
+                    curDir = os.getcwd() + ">>"
+                    client.sendm(curDir)
+                except Exception:
+                    client.sendm("error")
+            elif command[:4] == "down":
+                client.upload(command[5:])
+            elif command[:6] == "upload":
+                client.download(command[7:])
+            else:
+                cmd = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                output = cmd.stdout.read() + cmd.stderr.read()
+                output = str(output, "utf-8")
+                curDir = os.getcwd() + ">>"
+                client.sendm(output + curDir)
+                print(output)
+
+    if mode == "master":
+        masterMode()
+    elif mode == "slave":
+        slaveMode()
+
+
+def fileTransfrer(client):
+    def masterMode():
+        while True:
+            commandList = ['download', 'upload', 'back', 'open']
+            command = input().strip()
+            if command[:8] == "download":
+                pass
+            elif command[:6] == "upload":
+                pass
+            elif command[:4] == "back":
+                pass
+            elif command[:4] == "open":
+                pass
+            else:
+                print("Invalid Command")
+                print("valid commands:", commandList)
+
+    def slaveMode():
+        commandList = ['download', 'upload', 'back', 'open']
+        command = client.recv()
+        if command[:8] == "download":
+            pass
+        elif command[:6] == "upload":
+            pass
+        elif command[:4] == "back":
+            pass
+        elif command[:4] == "open":
+            pass
+        else:
+            print("Invalid Command")
+            print("valid commands:", commandList)
+
+
+def interface(client):
+    while True:
+        # print Menu
+        print("Select Mode :")
+        print("1. Chatting")
+        print("2. Remote Access (Master)")
+        print("3. Remote Access (Slave)")
+        print("0. Exit")
+        choice = int(input("choice :"))
+        # execute options
+        if choice == 1:
+            chatting(client)
+        elif choice == 2:
+            remoteAccess(client, "master")
+        elif choice == 3:
+            remoteAccess(client, "slave")
+        elif choice == 0:
+            exit()
+        else:
+            print("[-] Invalid selection")
+
+
+if __name__ == "__main__":
+    # def main():
+    client = Client()
+    client.connectHost()
+
+    interface(client)
